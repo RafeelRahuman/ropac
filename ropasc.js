93c909a6319e49371bd1ac65cb13641dcecb32fc
RafeelRahuman <mmrrrahuman@gmail.com>
2026-01-22 11:02:31 +0530

    Third comlete with UI

--- ropasc.js
+++ ropasc.js
@@ -13,20 +13,41 @@ function getComputerChoice(){
     }
 }
 
+const gameButtons = document.querySelectorAll("#container button")
+const resultDisplay = document.querySelector("#results")
+
+let score = document.querySelector("#scores")
+
+const statusDisplay =document.querySelector("#status")
+
+const reset = document.querySelector("#reset")
+
+gameButtons.forEach((button) =>  {
+    button.addEventListener("click" , () => {
+        const humanSelection=button.id;
+        const computerSelection=getComputerChoice();
+        playRound(humanSelection, computerSelection);
+   
+    });
+});
 
 
-function getHumanChoice(){
-    let hum=prompt("please enter rock or paper or scissor");
-    return hum ? hum.toLowerCase() : "";
-}
+
+
+// function getHumanChoice(){
+//     let hum=prompt("please enter rock or paper or scissor");
+//     return hum ? hum.toLowerCase() : "";
+// }
 
 let humanScore=0;
 let computerScore=0;
 
+
+
 function playRound(humanChoice,computerChoice){
 
     if(humanChoice === computerChoice){
-        console.log("Score none");
+          resultDisplay.textContent = "Game Tie"
     }
     else if (
         (humanChoice === "rock" && computerChoice === "scissor")||
@@ -34,33 +55,56 @@ function playRound(humanChoice,computerChoice){
         (humanChoice === "scissor" && computerChoice === "paper"))
         {
         humanScore+=1;
-        console.log("You WIN " + humanChoice + " Beats " + computerChoice);
+        resultDisplay.textContent ="Player WIN " + humanChoice + " Beats " + computerChoice;
     }
     else{
         computerScore+=1;
-        console.log("You Lose " + computerChoice + " Beats " + humanChoice)
+        resultDisplay.textContent = "Computer Win " + computerChoice + " Beats " + humanChoice;
     }
+
+    if (humanScore === 5 || computerScore === 5) {
+    if (humanScore === 5) {
+        statusDisplay.textContent = "Congratulations! You reached 5 points first.";
+    } else {
+        statusDisplay.textContent = "Game Over! The Computer reached 5 points first.";
+    }
+    gameButtons.forEach((btn) => btn.disabled = true);   
 }
 
 
+score.textContent = "Player: " + humanScore + " - Computer: " + computerScore;
 
-function playGame(){
-    for(let i=0;i<5;i++){
-const humanSelection=getHumanChoice();
-const computerSelection=getComputerChoice();
 
-playRound(humanSelection,computerSelection)}
-console.log("Human Score : " + humanScore + " Computer Score : " + computerScore)
-
-if(humanScore > computerScore){
-    console.log("WINNER is Human")
 }
-else if(computerScore > humanScore){
-    console.log("WINNER is Computer")
-}
-else{
-    console.log("Match Tied")
-}
-}   
 
-playGame();
+reset.addEventListener( "click" ,() => {
+    humanScore=0;
+    computerScore=0;
+    statusDisplay.textContent = ""
+    resultDisplay.textContent = ""
+    score.textContent = "Player: 0 - Computer: 0";
+    gameButtons.forEa ch((btn) => btn.disabled = false )
+})
+
+
+// function playGame(){
+//     for(let i=0;i<5;i++){
+// const humanSelection=getHumanChoice();
+// const computerSelection=getC omputerChoice();
+
+// playRound(humanSelection,computerSelection)
+// }
+// console.log("Human Score : " + humanScore + " Computer Score : " + computerScore)
+
+// if(humanScore > computerScore){
+//     console.log("WINNER is Human")
+// }
+// else if(computerScore > humanScore){
+//     console.log("WINNER is Computer")
+// }
+// else{
+//     console.log("Match Tied")
+// }
+// }   
+
+// playGame();
